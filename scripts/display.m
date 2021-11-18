5b293feeab37638ab1630925ba2106ca98106753
Eris Lund <38136789+0x5066@users.noreply.github.com>
2021-11-18 15:18:49 +0100

    dankpods easter eggs, about time

--- scripts/display.m
+++ scripts/display.m
@@ -3,13 +3,16 @@
 
 Global Group frameGroup;
 Global Togglebutton ShuffleBtn,RepeatBtn,ShuffleBtn2,RepeatBtn2;
-Global Timer SongTickerTimer;
+Global Timer SongTickerTimer, RandomTimer, nanoTimer;
 Global Text InfoTicker;
 Global GuiObject SongTicker;
 Global Slider Balance;
 Global Layout normal;
+Global int reallyrandom/*, i*/;
+Global String dankpods;
 
 function setSongtickerScrolling();
+//Function please();
 
 System.onScriptLoaded() {
 	initAttribs();
@@ -20,6 +23,14 @@ System.onScriptLoaded() {
 
 	SongTickerTimer = new Timer;
 	SongTickerTimer.setDelay(1000);
+
+	RandomTimer = new Timer;
+	RandomTimer.setDelay(500);
+	RandomTimer.start();
+
+	nanoTimer = new Timer;
+	nanoTimer.setDelay(500);
+	//i = 0;
 
 	RepeatBtn = frameGroup.findObject("Repeat");
 	ShuffleBtn = frameGroup.findObject("Shuffle");
@@ -46,6 +57,10 @@ normal.onAction (String action, String param, Int x, int y, int p1, int p2, GuiO
 	}
 }
 
+RandomTimer.onTimer(){
+	reallyrandom = random(9);
+}
+
 SongTickerTimer.onTimer() {
 	SongTicker.show();
 	InfoTicker.hide();
@@ -68,6 +83,59 @@ Balance.onSetPosition(int newpos)
 	InfoTicker.show();
 	InfoTicker.setText(t);
 }
+
+SongTicker.onLeftButtonDown (int x, int y)
+{
+	//i'm so sorry for this.
+	//reallyrandom = 8;
+
+	String extension = System.strlower(System.getExtension(System.removePath(System.getPlayItemString())));
+
+	if (isKeyDown(VK_ALT) && isKeyDown(VK_SHIFT) && isKeyDown(VK_CONTROL))
+	{
+	if(reallyrandom == 0) dankpods = "Can you believe no one bought this?";
+	else if(reallyrandom == 1) dankpods = "So like, this one time, yeh?";
+	else if(reallyrandom == 2) dankpods = "Someone's been in here.";
+	else if(reallyrandom == 3) dankpods = "It's like an MP3 player or something.";
+	else if(reallyrandom == 4) dankpods = "Oh Craig!";
+	else if(reallyrandom == 5) dankpods = "They sound like kids percussion.";
+	else if(reallyrandom == 6) dankpods = "We're off to cashies, mate!";
+	else if(reallyrandom == 7) dankpods = "na-no";
+	//else if(reallyrandom == 8) nanoTimer.start();
+	else if(reallyrandom == 8) dankpods = "Oh my PKcells...";
+	else if(reallyrandom == 9) dankpods = "Also known as: the bare minimum.";
+
+	if(extension == "mp3") dankpods = "MP3, a.k.a the bare minimum.";
+	SongTickerTimer.start();
+	SongTicker.hide();
+	InfoTicker.show();
+	InfoTicker.setText(dankpods);
+	}
+}
+
+/*
+nanoTimer.onTimer(){
+	//this is bad, there's probably a better way
+	i++;
+	if(i >= 3){
+		i = 0;
+	}
+
+	if(i == 0){
+		dankpods = "na   ";
+	}else if(i == 1){
+		dankpods = "na-no";
+	}
+	please();
+	InfoTicker.setText(dankpods);
+}
+
+Please(){
+	if(i == 2){
+		nanoTimer.stop();
+	}
+}
+*/
 
 RepeatBtn.onToggle(boolean on) {
 	SongTickerTimer.start();
