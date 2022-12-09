06e572d2d5ab5160b83e45b261057cc5768981eb
Eris Lund <38136789+0x5066@users.noreply.github.com>
2022-12-09 20:07:13 +0100

    add font detection to holiday edition

--- scripts/Holiday Edition Specific/titlebar.m
+++ scripts/Holiday Edition Specific/titlebar.m
@@ -1,6 +1,8 @@
 #include "..\..\..\lib/std.mi"
-
+#include "..\..\..\lib\fileio.mi"
 #define MARGIN 0
+
+Global File Arial;
 
 Function resizeObjects();
 
@@ -13,6 +15,9 @@ Global int padleft, padright;
 Global Boolean IsWine;
 
 System.onScriptLoaded() {
+  Arial = new File;
+  Arial.load("Z:\\usr\\share\\fonts\\TTF\\arial.ttf");
+  Arial.exists();
 
   IsWine = stringToInteger(getToken(getParam(), ";", 3));
 
@@ -38,7 +43,11 @@ System.onScriptLoaded() {
   holly_r = sg.findObject("player.main.xmasholly.r");
 
   if(IsWine != NULL){
-    tcenter.setXmlParam("y", "3");
+    if(Arial.exists() == 1){
+      //do nothing, because the font looks good already
+    }else{
+      tcenter.setXmlParam("y", "3");
+    }
   }
 }
 
